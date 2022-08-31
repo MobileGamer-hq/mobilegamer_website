f4359407346135ba4c6595fdc53702ce9ea09bba
MobileGamer-hq <73354088+MobileGamer-hq@users.noreply.github.com>
2022-08-31 22:12:50 +0100

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import About from './component/About/About'
+import Experience from './component/Experience/Experience'
+import Footer from './component/Footer/Footer'
+import Home from './component/Home/Home'
+import Navigation from './component/Navigation/Navigation'
+import Projects from './component/Projects/Projects'
+
 
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Home/>
+      <Navigation/>
+      <About/>
+      <Experience/>
+      <Projects/>
+      <Footer/>
+    </>
   )
 }
 
